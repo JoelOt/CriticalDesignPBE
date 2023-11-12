69049511aca6eba18379c8102329bff1527ac8d0
JosepPella <146730156+JosepPella@users.noreply.github.com>
2023-11-12 15:34:59 +0100

    Update RequestHandler.py

--- RequestHandler.py
+++ RequestHandler.py
@@ -11,7 +11,7 @@ class RequestHandler():
 
   #url destinat a text que ha de tornar rq que pot ser una llista on string amb el nom de l'usuari ... 
   def request(self, text):
-    url = "http://localhost:8080/phpProject2/back/" + text
+    url = "http://localhost:8080/CriticalDesignPBE" + text
     response = requests.get(url)
     rq = response.json()
     return (rq)
