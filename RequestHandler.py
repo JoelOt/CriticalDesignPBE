138509ce0c1e358c92371bc417dc0fbf0f23b277
JosepPella <146730156+JosepPella@users.noreply.github.com>
2023-11-12 14:56:45 +0100

    Update RequestHandler.py

--- RequestHandler.py
+++ RequestHandler.py
@@ -5,12 +5,12 @@ import json
 
 class RequestHandler():
   def __init__(self):
-      self.thread = threading.Thread(target=self.request_str)
+      self.thread = threading.Thread(target=self.request)
       self.thread.start()
 
 
   #url destinat a text que ha de tornar rq que pot ser una llista on string amb el nom de l'usuari ... 
-  def request_strt(self, text):
+  def request(self, text):
     url = "http://localhost:8080/phpProject2/back/" + text
     response = requests.get(url)
     rq = response.json()
