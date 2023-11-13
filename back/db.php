bf8de9399c130dff1456abd66bd61fa21dffc3a9
JoelOtero <119890772+JoelOteroMas@users.noreply.github.com>
2023-11-13 01:30:07 +0100

    Add files via upload

--- back/db.php
+++ back/db.php
@@ -1,8 +1,8 @@
 <?php  //conecta la base de dades
 $conn = mysqli_connect(
     'localhost',  //nom server
-    'test',  //nom user de la db
-    'test',  //contrasenya de la db
+    'root',  //nom user de la db
+    '',  //contrasenya de la db
     'PBE_BD'  //nom de la db
 );
 ?>
