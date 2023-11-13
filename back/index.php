420d04754db29c86688f1e968fbd014d71830280
JoelOtero <119890772+JoelOteroMas@users.noreply.github.com>
2023-11-13 01:30:23 +0100

    Add files via upload

--- back/index.php
+++ back/index.php
@@ -7,31 +7,30 @@ if(isset($_GET['request'])){ // si s'ha enviat el timetables:
         $consulta = "SELECT * FROM timetables";
         if(isset($_GET['day'])){ //si es timetables hem de mirar si ha especificat day i hour
             $day = $_GET['day'];
-            $consulta .= "WHERE day = '$day'";  //afegim a la consulta el filtre de dia
+            $consulta .= " WHERE day = '$day'";  //afegim a la consulta el filtre de dia
             if(isset($_GET['hour'])){
                 $hour = $_GET['hour'];
                 $consulta .= "AND hour = '$hour'";
-
             }
         }elseif(isset($_GET['limit'])){
             $limit = $_GET['limit'];
-            $consulta .= "LIMIT $limit"; 
+            $consulta .= " LIMIT $limit"; 
         }
     }
+
     if($request == 'marks'){  //a fer
-        $consulta = "SELECT * FROM marks";
+        $consulta = "SELECT subject FROM marks";
     }
     if($request == 'tasks'){  //a fer
         $consulta = "SELECT * FROM tasks";
     }
 
     $resultat = mysqli_query($conn, $consulta);  #executa la consulta $consulta a la db $conn
-
-    $resultat_array = array();
+    $data= array();
     while($row = mysqli_fetch_assoc($resultat)){
-        $resultat_array[] = $row;
+        $data[] = $row;
     }
     header('Content-Type: application/json');
-    echo json_encode($resultat_array);
+    echo json_encode($data);
 }
 ?>
