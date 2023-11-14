7c95cd7047fd4153ff7478b8926abd78935be012
JoelOtero <omasjo11@gmail.com>
2023-11-14 22:42:45 +0100

    ie

--- back/index.php
+++ back/index.php
@@ -2,16 +2,16 @@
 include("db.php"); //include tira el codi de l'arxiu que dius, aquest obre la db
 
 if(isset($_GET['request'])){ // si s'ha enviat el timetables:
-    $request = $_GET['request'];  //pot ser timetables, tasks, marks
+    $request = $_GET['request'];  //pot ser timetables, tasks, marks, students
     if($request == 'timetables'){   //falta el limit
         $consulta = "SELECT * FROM timetables";
         if(isset($_GET['day'])){ //si es timetables hem de mirar si ha especificat day i hour
             $day = $_GET['day'];
             $consulta .= " WHERE day = '$day'";  //afegim a la consulta el filtre de dia
-            if(isset($_GET['hour'])){
+            if(isset(_GET['hour'])){
                 $hour = $_GET['hour'];
-                $consulta .= "AND hour = '$hour'";
             }
+            $consulta .= " AND hour = '$hour'";
         }elseif(isset($_GET['limit'])){
             $limit = $_GET['limit'];
             $consulta .= " LIMIT $limit"; 
@@ -19,12 +19,29 @@ if(isset($_GET['request'])){ // si s'ha enviat el timetables:
     }
 
     if($request == 'marks'){  //a fer
-        $consulta = "SELECT subject FROM marks";
-    }
-    if($request == 'tasks'){  //a fer
-        $consulta = "SELECT * FROM tasks";
+        $consulta = "SELECT * FROM marks";
+        if(isset($_GET['subject'])){
+            $subject = $_GET['subject'];
+            $consulta .= " WHERE subject = '$subject'";
+        }if(isset($_GET['mark[lt]'])){
+            $mark = $_GET['mark[lt]'];
+            $consulta .= " WHERE mark < '$mark'";
+        }
     }
 
+    if($request == 'tasks'){  //a fer
+        $consulta = "SELECT * FROM tasks";
+        if(isset($_GET['date'])){
+            $date = $_GET['date'];
+        }if(isset($_GET['hour[gte]'])){  //no entenc que vol dir el gte
+            $date = $_GET['date[gte]'];
+        }
+        $consulta .= " WHERE date = '$date'";
+        if(isset($_GET['limit'])){
+            $limit = $_GET['limit'];
+            $consulta .= " LIMIT $limit"; 
+        }
+    }
     $resultat = mysqli_query($conn, $consulta);  #executa la consulta $consulta a la db $conn
     $data= array();
     while($row = mysqli_fetch_assoc($resultat)){
