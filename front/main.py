906002b7d1b0f1747d865a485aa987e99dfafac0
ariadnamarcos <145438133+ariadnamarcos@users.noreply.github.com>
2023-11-14 10:15:34 +0100

    Update main.py
    Aquesta versió del main és la més reduida, només crea la finestra i instancia els objectes CourseHandler i EntryHandler i poc mes

--- front/main.py
+++ front/main.py
@@ -1,4 +1,29 @@
-import RequestHandler
-import CourseManager
+import sys
 
-#fil principal de execució que inicia el codi grafic CourseManager i fa les crides del RequestHandler
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, Gdk
+from course_manager import CourseManager
+from entry_handler import EntryHandler
+
+
+def handle_request_result(result):
+    print(result)
+
+
+
+if __name__ == '__main__':
+    
+    entry = Gtk.Entry()
+    entry_handler = EntryHandler(entry, handle_request_result)
+    course_manager = CourseManager(entry_handler)
+
+    win = Gtk.Window()
+    Gtk.Window.__init__(win, title = 'Course Manager')
+    win.set_default_size(800, 500)
+    win.connect("destroy", Gtk.main_quit)
+    win.add(course_manager)
+    win.show_all()
+    Gtk.main()
+
