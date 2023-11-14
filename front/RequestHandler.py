4679034e81cfc070fe2c7adc60aacffc059cbce0
ariadnamarcos <145438133+ariadnamarcos@users.noreply.github.com>
2023-11-14 10:20:05 +0100

    Update entry_handler.py
    he posat al mateix fitxer les classes RequestHandler i EntryHandler per no tenir tants fitxers.
    El mètode entry_handler detecta els canvis que es fan al text d'entrada (es a dir, detecta qualsevol cosa que escriguis), pero després no se gaire el que fa perq el request no el controlo molt. Suposo que es allo de que el url no apunta enlloc o algo aixi

--- front/RequestHandler.py
+++ front/RequestHandler.py
@@ -1,17 +1,39 @@
-import threading
+import sys
+
+import gi
+import threading 
 import requests
 import json
 
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, Gdk
 
-class RequestHandler(text):
-  def __init__(self):
-      self.text = text;
+class EntryHandler:
+    def __init__(self, entry, callback):
+        self.entry = entry
+        self.callback = callback
+        self.entry.connect("changed", self.on_entry_changed)
+
+    def on_entry_changed(self, entry):
+        text = entry.get_text()
+        if text:
+            handler = RequestHandler(text, self.callback)
+            handler.thread.start()
+
+
+
+class RequestHandler:
+  def __init__(self, text, callback):
+      self.text = text
+      self.callback = callback
       self.thread = threading.Thread(target=self.request)
       self.thread.start()
 
   def request(self):
-    request = self.text.get().split("?")
-    url = "http://localhost:8080/CriticalDesignPBE/back/index.php?request={}&{}".format(request[0], request[1])
+    request_data = self.text.get().split("?")
+    url = "http://localhost:8080/CriticalDesignPBE/back/index.php?request={}&{}".format(request_data[0], request_data[1])
     response = requests.get(url)
-    rq = response.json()
-    return (rq)
+    result = response.json()
+    self.callback(result)
+
+
