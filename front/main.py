0b73187d844cbcac0c52a29ee4ebb6e83866d5d8
celiatorras <145282982+celiatorras@users.noreply.github.com>
2023-11-15 10:35:40 +0100

    Update main.py

--- front/main.py
+++ front/main.py
@@ -39,25 +39,19 @@ class CourseManager(Gtk.Grid):
         thread1 = threading.Thread(target= self.consultarServer(text))  #li passem el que esta escrit
         thread1.start()
         
-    def consultarServer(self,text):  #fa la consulta GET i rep com a resultat un json 
-        request_data = text.split("?")
-        tables = request_data[0]
-        variables = request_data[1]
-        textvar = ""
-        if (len(request_data) >= 2){
-            textvar = tables + "&" + variables
-        } else if (){
-            textvar = tables
-        }else{
-            return
-        }
-        url = "http://localhost:8080/CriticalDesignPBE/back/index.php?" + textvar
-        response = requests.get(url)
-        result = response.json()
+    def consultarServer(self):  #fa la consulta GET i rep com a resultat un json 
+        request = self.request.get().split("?")
+        if len(request) < 2:
+            request.append('')
+        url = "http://localhost:8080/CriticalDesignPBE/back/index.php?request={}&{}".format(request[0], request[1])  #url de l'arxiu index.php on s'envia la request per que el processi
+        response = requests.get(url)  #fem una request post a l'url enviant les data
+        print("STATUS: {}, url: {}".format(response.status_code, url))
+        # verificar si la request és exitosa
+        if response.status_code == 200: #200 exitosa, 404 url no trobat, 500 error en el php...
+            result = response.json()
+            GObject.idle_add(self.update_ui, result)  #modifica la interficie grafica 
         
-        GObject.idle_add(self.update_ui, result)  #modifica la interficie grafica
-        
-    def update_ui(self,result):
+    def update_ui(self, result):
         #posar les dades maques
         array_of_strings = [json.dumps(entry) for entry in result]
         for entry_str in array_of_strings:
@@ -77,50 +71,3 @@ if __name__ == '__main__':
     win.add(course_manager)
     win.show_all()
     Gtk.main()
-
-    
-    
-    def metodeThread(self, widjet):  #creem un thread per consultar el server de forma concurrent
-        text = entry.get_text()
-        thread1 = threading.Thread(target= self.consultarServer(text))  #li passem el que esta escrit
-        thread1.start()
-        
-    def consultarServer(self,text):  #fa la consulta GET i rep com a resultat un json 
-        request_data = text.split("?")
-        tables = request_data[0]
-        variables = request_data[1]
-        textvar = ""
-        if (len(request_data) >= 2){
-            textvar = tables + "&" + variables
-        } else if (){
-            textvar = tables
-        }else{
-            return
-        }
-        url = "http://localhost:8080/CriticalDesignPBE/back/index.php?" + textvar
-        response = requests.get(url)
-        result = response.json()
-        
-        GObject.idle_add(self.update_ui, result)  #modifica la interficie grafica
-        
-    def update_ui(self,result):
-        #posar les dades maques
-        array_of_strings = [json.dumps(entry) for entry in result]
-        for entry_str in array_of_strings:
-            entry_dict = json.loads(entry_str)
-            
-        #modifciar taula
-        
-if __name__ == '__main__':
-    
-    entry = Gtk.Entry()
-    course_manager = CourseManager()
-
-    win = Gtk.Window()
-    Gtk.Window.__init__(win, title = 'Course Manager')
-    win.set_default_size(800, 500)
-    win.connect("destroy", Gtk.main_quit)
-    win.add(course_manager)
-    win.show_all()
-    Gtk.main()
-
