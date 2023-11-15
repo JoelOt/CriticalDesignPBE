3c8035837aa8f2d2f159da4d689f581b3def4fa4
JosepPella <146730156+JosepPella@users.noreply.github.com>
2023-11-15 08:46:10 +0100

    Update RequestHandler.py

--- front/RequestHandler.py
+++ front/RequestHandler.py
@@ -14,36 +14,25 @@ class EntryHandler:
         self.callback = callback
         self.entry.connect("changed", self.on_entry_changed)
 
-    def on_entry_changed(self, entry):
-        text = entry.get_text()
+    def on_entry_changed(self):
         if text:
-            handler = RequestHandler(text, self.callback)
-            handler.thread.start()
-
-
-
-class RequestHandler:
-    def __init__(self, text, callback):
-        self.text = text
-        self.callback = callback
-        self.thread = threading.Thread(target=self.request)
-        self.thread.start()
-
-  def request(self):
-      request_data = self.text.get().split("?")
-      tables = request_data[0]
-      variables = request_data[1]
-      textvar = ""
-      if (len(request_data) >= 2){
-          textvar = tables + "&" + variables
-      } else if (){
-          textvar = tables
-      }else{
-          return
-      }
-      url = "http://localhost:8080/CriticalDesignPBE/back/index.php?" + textvar
-      response = requests.get(url)
-      result = response.json()
-      self.callback(result)
+            self.thread = threading.Thread(target=self.request)
+            self.thread.start()
+    def request(self):
+        request_data = self.entry.split("?")
+        tables = request_data[0]
+        variables = request_data[1]
+        textvar = ""
+        if (len(request_data) >= 2){
+            textvar = tables + "&" + variables
+        } else if (){
+            textvar = tables
+        }else{
+            return
+        }
+        url = "http://localhost:8080/CriticalDesignPBE/back/index.php?" + textvar
+        response = requests.get(url)
+        result = response.json()
+        self.callback(result)
 
 
