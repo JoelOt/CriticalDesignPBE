7073d2c32ea6f9974453e0a879ce2d47d8a940a2
celiatorras <145282982+celiatorras@users.noreply.github.com>
2023-11-15 10:16:52 +0100

    Update CourseManager.py

--- front/CourseManager.py
+++ front/CourseManager.py
@@ -4,32 +4,32 @@ import gi
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk
-from entry_handler import EntryHandler
+from RequestHandler import EntryHandler
 
-class CourseManager(Gtk.Box):
+
+
+class CourseManager(Gtk.Grid):
     def __init__(self, entry_handler):
         super().__init__()
 
-        self.set_orientation(Gtk.Orientation.VERTICAL)
-        self.set_spacing(150)
-
+        self.set_size_request(-1, 20)
         self.create_button()
         self.create_entry(entry_handler)
 
         self.show_all()
 
     def create_button(self):
-        self.button = Gtk.Button(label = 'Logout')
-        self.button.set_size_request(60, 40)
-        self.button.set_alignment(1, 0)
-        self.pack_start(self.button, False, False, 10)
+        button = Gtk.Button(label = 'Logout')
+        self.attach(button,1, 0, 1, 1)
         
-    
-
     def create_entry(self, entry_handler):
         entry = Gtk.Entry()
         entry.set_placeholder_text("Enter your query:")
         entry.connect("changed", entry_handler.on_entry_changed)
+        self.attach(entry,0, 1, 1, 1)
+
+
+
         self.pack_start(entry, False, False, 0)
 
 
