9a141c424069ec58aba9c399f5a5402f7c08d9b3
ariadnamarcos <145438133+ariadnamarcos@users.noreply.github.com>
2023-11-17 23:09:28 +0100

    Update main.py

--- front/main.py
+++ front/main.py
@@ -5,7 +5,7 @@ import json
 import requests
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, Gdk
+from gi.repository import Gtk, GObject, GLib
 
 
 class CourseManager(Gtk.Grid):
@@ -18,17 +18,17 @@ class CourseManager(Gtk.Grid):
         self.create_entry()
         self.create_grid()
         
-        self.login()
+        #self.login()
     
     def create_button(self):
         self.button = Gtk.Button(label = 'Logout')
         self.attach(self.button,1, 0, 1, 1)
-        self.button.connect("clicked", self.login)
+     #   self.button.connect("clicked", self.login)
         
     def create_entry(self):
         self.entry = Gtk.Entry()
         self.entry.set_placeholder_text("Enter your query:")
-        self.entry.connect("changed", metodeThread)
+        self.entry.connect("activate", self.metodeThread)
         self.attach(self.entry,0, 1, 1, 1)
 
     def create_grid(self):
@@ -38,34 +38,14 @@ class CourseManager(Gtk.Grid):
         self.attach(self.grid, 0, 2, 2, 1)
 
 
-    def login (self):  #crec que ja ens convé que sigui bloquejant
-        self.hide_all()
-        label = Gtk.Label(label="acerque su tarjeta")
-        self.attach(label,0, 1, 1, 1)
-        label.show()
-
-        #llegir uid
-        
-        label.set_text(uid)
-        
-        url = "http://localhost:8080/CriticalDesignPBE/back/index.php?uid={}".format(uid)
-        response = requests.get(url)
-        if response.status_code == 200:
-            result = response.text()
-            #ensenyar al lcd en thread auxiliar
-        label.destroy()
-        slef.entry.show()
-        slef.button.show()
-
-    
-
-    def metodeThread(self, widjet):  #creem un thread per consultar el server de forma concurrent
+    def metodeThread(self, widget):  #creem un thread per consultar el server de forma concurrent
         text = self.entry.get_text()
         thread1 = threading.Thread(target= self.consultarServer(text))  #li passem el que esta escrit
         thread1.start()
         
     def consultarServer(self, text):  #fa la consulta GET i rep com a resultat un json 
         request = text.split("?")
+        self.req = request[0]
         if len(request) < 2:
             request.append('')
         url = "http://localhost:8080/CriticalDesignPBE/back/index.php?request={}&{}".format(request[0], request[1])  #url de l'arxiu index.php on s'envia la request per que el processi
@@ -74,22 +54,40 @@ class CourseManager(Gtk.Grid):
         # verificar si la request és exitosa
         if response.status_code == 200: #200 exitosa, 404 url no trobat, 500 error en el php...
             result = response.text
-            GObject.idle_add(self.update_ui, result)  #modifica la interficie grafica 
+            GLib.idle_add(self.update_ui, result)  #modifica la interficie grafica
+            self.grid = CourseManager()
+            self.grid.__init__()
+
         
     def update_ui(self, result):
-        matriz = json.loads(result)
+        try:
+            matriz = json.loads(result)
+        except json.decoder.JSONDecodeError as e:
+            print("Error al decodificar JSON:", e)
+            return
         
-        for i, fila in enumerate(matriz):
-            for j, dada in enumerate(fila):
-                label = Gtk.Label(label=str(dada))
-                self.grid.attach(label, j, i, 1, 1)
-                
-        self.grid.show()
 
+        if self.req == 'marks':
+            labels = ['subject', 'name', 'mark', 'id']
+        elif self.req == 'timetables':
+            labels = ['day', 'hour', 'subject', 'room']
+        elif self.req == 'tasks':
+            labels = ['date', 'subject', 'name']
 
+        for i, label_text in enumerate(labels):
+            label = Gtk.Label(label=label_text)
+            self.grid.attach(label, i, 2, 3, 3)
+
+        for row, person in enumerate(matriz):
+            for col, key in enumerate(labels):
+                value = person[key]
+                label = Gtk.Label(label=value)
+                self.attach(label, col, row + 1, 1, 1)
+        
+        
+        win.show_all()
 if __name__ == '__main__':
     
-    entry = Gtk.Entry()
     course_manager = CourseManager()
 
     win = Gtk.Window()
