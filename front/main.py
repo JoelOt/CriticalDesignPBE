351236c697b39488d948521862132dfc19397912
ariadnamarcos <145438133+ariadnamarcos@users.noreply.github.com>
2023-11-18 19:26:19 +0100

    Update main.py

--- front/main.py
+++ front/main.py
@@ -5,7 +5,7 @@ import json
 import requests
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, GObject, GLib
+from gi.repository import Gtk, Gdk, GLib
 
 
 class CourseManager(Gtk.Grid):
@@ -23,6 +23,8 @@ class CourseManager(Gtk.Grid):
     def create_button(self):
         self.button = Gtk.Button(label = 'Logout')
         self.attach(self.button,1, 0, 1, 1)
+        estil = self.button.get_style_context()
+        estil.add_class("button_style")
      #   self.button.connect("clicked", self.login)
         
     def create_entry(self):
@@ -37,12 +39,11 @@ class CourseManager(Gtk.Grid):
         self.grid.set_column_homogeneous(True)
         self.attach(self.grid, 0, 2, 2, 1)
 
-
     def metodeThread(self, widget):  #creem un thread per consultar el server de forma concurrent
         text = self.entry.get_text()
         thread1 = threading.Thread(target= self.consultarServer(text))  #li passem el que esta escrit
         thread1.start()
-        
+      
     def consultarServer(self, text):  #fa la consulta GET i rep com a resultat un json 
         request = text.split("?")
         self.req = request[0]
@@ -55,9 +56,6 @@ class CourseManager(Gtk.Grid):
         if response.status_code == 200: #200 exitosa, 404 url no trobat, 500 error en el php...
             result = response.text
             GLib.idle_add(self.update_ui, result)  #modifica la interficie grafica
-            self.grid = CourseManager()
-            self.grid.__init__()
-
         
     def update_ui(self, result):
         try:
@@ -66,7 +64,6 @@ class CourseManager(Gtk.Grid):
             print("Error al decodificar JSON:", e)
             return
         
-
         if self.req == 'marks':
             labels = ['subject', 'name', 'mark', 'id']
         elif self.req == 'timetables':
@@ -74,26 +71,42 @@ class CourseManager(Gtk.Grid):
         elif self.req == 'tasks':
             labels = ['date', 'subject', 'name']
 
-        for i, label_text in enumerate(labels):
-            label = Gtk.Label(label=label_text)
-            self.grid.attach(label, i, 2, 3, 3)
+        win.__init__()
 
+      
         for row, person in enumerate(matriz):
             for col, key in enumerate(labels):
+                if row == 0:
+                    label = Gtk.Label(label=key)
+                    estil = label.get_style_context()
+                    estil.add_class("personalitzar")
+                    self.attach(label, col , 3, 1, 1)
                 value = person[key]
                 label = Gtk.Label(label=value)
-                self.attach(label, col, row + 1, 1, 1)
-        
-        
+                estil = label.get_style_context()
+                estil.add_class("personalitzar")
+                self.attach(label, col, row + 5 , 1, 1)
+                label.set_hexpand(True)
+
         win.show_all()
+
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        super().__init__(title = 'Course Manager')
+        self.set_default_size(800, 500)
+        self.connect("destroy", Gtk.main_quit)
+
 if __name__ == '__main__':
     
     course_manager = CourseManager()
-
-    win = Gtk.Window()
-    Gtk.Window.__init__(win, title = 'Course Manager')
-    win.set_default_size(800, 500)
-    win.connect("destroy", Gtk.main_quit)
+    css_provider = Gtk.CssProvider()
+    css_provider.load_from_path("style.css")
+ 
+    win = MyWindow()
+    win.__init__()
     win.add(course_manager)
+    screen= Gdk.Screen.get_default()
+    style_context = Gtk.StyleContext()
+    style_context.add_provider_for_screen(screen, css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
     win.show_all()
     Gtk.main()
