b90fbb384cc5a2b2a2296480597a935504ba3eea
JosepPella <146730156+JosepPella@users.noreply.github.com>
2023-12-08 13:33:13 +0100

    Update index2.php

--- back/index2.php
+++ back/index2.php
@@ -14,15 +14,27 @@ $table = ltrim($_SERVER['PATH_INFO'], '/'); //s'ha de treure el '/' devant de la
 $consulta = "SELECT * FROM {$table}";  
 if($_SERVER['QUERY_STRING']!= NULL){
     $query = explode('&', $_SERVER['QUERY_STRING']); //quedarà un array [day=Fri, hour=8:00, subject=PBE]
-    
+
     $params = parse_query($query);  //Array ( [day] => FRi, [hour] => 8:00, [subject] => PBE )
     $primeraCondicio = true;
     foreach($params as $key => $value){
         if(isset($key)){
             $consulta .= ($primeraCondicio ? " WHERE" : " AND");  //tria WHERE si &primeraciondició=True
             $primeraCondicio = false;  
-            
-            $consulta .=" $key = '$value'";
+ 
+            $keyParts = explode('[', $key);     //en aquesta secció de codi es on posem les codicións lt and gt
+            if (isset($keyParts[1])) {
+                $cond = trim($keyParts[1],']');
+                $key0 = $keyParts[0];
+                if($cond =='gt'){
+                    $consulta .=" $key0 > '$value'";
+                }else if ($cond =='lt'){
+                    $consulta .=" $key0 < '$value'";
+                }                               //si volem posar mes condicionants només hem d'afegir else if
+            }else{
+                $consulta .=" $key = '$value'";
+
+            }
         }
     }
 }
