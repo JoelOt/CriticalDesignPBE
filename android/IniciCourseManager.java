667eb100f28b2da56dd85daf8247b634fca28049
JosepPella <146730156+JosepPella@users.noreply.github.com>
2023-12-14 21:40:30 +0100

    Update IniciCourseManager.java

--- android/IniciCourseManager.java
+++ android/IniciCourseManager.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.cdr.R;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
